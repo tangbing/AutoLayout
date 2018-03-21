a670f3246313fd75653ae911ef15d871c7fc988f
tangbing <bing.tang@newb-it.com>
2018-03-21 23:13:19 +0800

    添加一些理解注释

--- 约束优先级/约束优先级/ViewController.m
+++ 约束优先级/约束优先级/ViewController.m
@@ -24,10 +24,15 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    [self.greenView removeFromSuperview];
+  
     
-    //实现lable包裹文字：大于180，换行显示，小于180，完全包裹文字，设置lable的宽度约束最大约束为180
+    //1.实现lable包裹文字：大于180，换行显示，小于180，完全包裹文字，设置lable的宽度约束最大约束为180
     self.label.text = @"lablelabel";
+    
+    // 实现蓝色viewremove掉,灰色的view调到原来蓝色view的位置
+    //约束优先级：优先级越高的，先执行，优先级越高的约束失效后，优先级低的开始生效。
+    
+      [self.greenView removeFromSuperview];
     
 }
 
